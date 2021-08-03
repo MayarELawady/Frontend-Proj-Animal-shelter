20837bbb64b04e50db38480348793c0d15c1f262
MayarElawady <mayarhanyelawady@gmail.com>
2021-08-03 03:12:15 +0200

    Upload Entire Project

---
+++ js/blogs.js
@@ -0,0 +1,29 @@
+//$(document).ready(function(){
+//    $(".new .walk").hide();
+//
+//    $("button").click(function(){
+//        $(".new .walk").show();
+//    });
+//});
+//$(document).ready(function(){
+//    $(".news .newspre").hide();
+//
+//    $("button").click(function(){
+//        $(".news .newspre").show();
+//    });
+//});
+//$(document).ready(function(){
+//    $(".news").hide();
+//
+//    $("button").click(function(){
+//        $(".news").show();
+//    });
+//});
+//});
+$(document).ready(function(){
+    $(".new").hide();
+
+    $("button").click(function(){
+        $(".new").show();
+    });
+});
